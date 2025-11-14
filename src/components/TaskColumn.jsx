447c692b05a6cd9baba158a4faa219d7645420e1
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-14 19:28:31 +0530

    develop and style taskcolumn component

---
+++ src/components/TaskColumn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from "../assets/direct-hit.png";
+import "./TaskColumn.css";
+export const TaskColumn = ({name,icon}) => {
+  return (
+    <section className="task_column">
+      <h2 className="task_column_heading">
+        <img className="task_column_icon"src={icon} alt="to do"/>{name}
+    </h2>
+    </section>
+  )
+}
